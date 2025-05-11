13d3696a82f2110d41c529d8474ded093a450bf2
bielzz698 <biel.ferreira3938@gmail.com>
2025-05-11 16:17:40 +0000

    novo super trunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    printl ("desafio super trunfo!\n");
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
